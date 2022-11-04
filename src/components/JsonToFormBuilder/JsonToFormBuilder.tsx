71831ad53fa2d61f6e2ff2a4865253ea7d20fd9b
as672379 <andrii.shedko@broadcom.com>
2022-11-04 16:27:43 +0100

    Small changes

--- src/components/JsonToFormBuilder/JsonToFormBuilder.tsx
+++ src/components/JsonToFormBuilder/JsonToFormBuilder.tsx
@@ -6,6 +6,12 @@ import { Result } from '../Result';
 export const JsonToFormBuilder = (): JSX.Element => {
   return (
     <div className='container'>
+      <div className='box'>
+        <h1>JSON to form</h1>
+        This application allows you to create HTML form from the JSON file. <br />
+        The example of the expected input: <br />
+        <img src='./screenshot.png' alt='input example' />
+      </div>
       <div className='columns'>
         <div className='column'>
           <JsonInput />
